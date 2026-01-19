aab005eae963b1e7fff8907cb69dfb6953bdca46
Thomas <xthomaspaulx@gmail.com>
2026-01-19 23:10:07 +0530

    folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import dynamic from 'next/dynamic';
 
 // 1. Setup the dynamic import for the 3D Scene
-const Scene3D = dynamic(() => import('@/app/components/canvas/Scene'), {
+const Scene3D = dynamic(() => import('@/components/canvas/Scene'), {
   ssr: false, 
   loading: () => <div className="flex h-full items-center justify-center text-white">Loading Advay Experience...</div>,
 });
