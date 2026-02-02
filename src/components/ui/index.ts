68905707b6c6d23f91c156993f7c63fe62a3e262
SahilShameerDev <smsahilms346@gmail.com>
2026-02-02 21:21:00 +0530

    Error fixed again

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -7,4 +7,4 @@ export { default as MobileMenu } from './MobileMenu'
 export { default as Navbar } from './Navbar'
 export { default as HeroContent } from './HeroContent'
 export { GooeyText } from './GooeyText'
-q
+export { default as Events } from './events_section/Events'
