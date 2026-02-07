3e9e39ff229ec583a037a79404fedf64ed0874f7
Mulearn-TIST <mulearn.tist@gmail.com>
2026-02-07 17:15:59 +0530

    More events URL

--- src/components/ui/events_section/Events.tsx
+++ src/components/ui/events_section/Events.tsx
@@ -806,7 +806,7 @@ export default function Events() {
                       preload="auto"
                       /* THIS LINE PREVENTS THE DOWNLOAD MENU */
                       onContextMenu={(e) => e.preventDefault()}
-                      onEnded={() => window.location.href = "./Events"}
+                      onEnded={() => window.location.href = "./events"}
                       style={{ 
                         opacity: isPressed3 ? 1 : 0,
                         transition: 'opacity 0.15s ease-out',
